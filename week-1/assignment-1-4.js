8e7fcfe3b3521b65c29ea42919c6868137f1057f
Jason Cords <jmcords@my365.bellevue.edu>
2020-02-17 17:33:01 -0600

    Assignment 1.4

---
+++ week-1/assignment-1-4.js
@@ -0,0 +1,22 @@
+/*
+============================================
+; Title: Assignment 1.4
+; Author: Jason Cords
+; Date: 17 Feb 2020
+; Description: Using primitive types
+;===========================================
+*/ 
+
+var firstName = "Jason";        /* variable for first name */
+var lastName = "Cords";         /* variable for last name */
+var age = "30";                 /* variable for age */
+var street = "123 Some St.";    /* variable for street */
+var city = "Anywhereville";     /* variable for city */
+var zipCode = "90210";          /* variable for zip code */
+
+console.log(firstName);         /* log command for first name*/
+console.log(lastName);          /* log command for last name */
+console.log(age);               /* log command for age */
+console.log(street);            /* log command for street */
+console.log(city);              /* log command for city */
+console.log(zipCode);           /* log command for zip code */
