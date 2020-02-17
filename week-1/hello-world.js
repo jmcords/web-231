8834c1f78a434f0e6c76a48f166b008810f35fe1
Jason Cords <jmcords@my365.bellevue.edu>
2020-02-17 17:10:31 -0600

    exercise 1.3

---
+++ week-1/hello-world.js
@@ -0,0 +1,9 @@
+/*
+============================================
+; Title: Exercise 1.3
+; Author: Jason Cords
+; Date: 17 Feb 2020
+; Description: Using hello world
+;===========================================
+*/ 
+console.log("Hello World, my name is Jason Cords"); 
