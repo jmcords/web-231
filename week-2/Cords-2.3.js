316113174be7bd73d91abc98560ce695d20d49ed
Jason Cords <jmcords@my365.bellevue.edu>
2020-02-23 16:48:09 -0600

    Exercise 2.3

---
+++ week-2/Cords-2.3.js
@@ -0,0 +1,21 @@
+
+/*
+============================================
+; Title:  Exercise 2.3.js
+; Author: Professor Krasso
+; Date:   23 Feb 2020
+; Modified by: Jason Cords
+; Description: Displays a formatted header
+;===========================================
+*/
+
+const header = require('../Cords-header.js');
+
+console.log(header.display("Jason", "Cords", "Exercise 2.3"));
+
+function myName(){ //creates function with zero parameters
+  return myName.Jason; //returns property created below
+}
+myName.Jason = "Jason Cords"; //creates function property
+
+console.log("\nHello", myName(),"!"); //logs property that was returned by the function
