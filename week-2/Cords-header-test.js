631118710158f9a4476a0334a06d61582f535a14
Jason Cords <jmcords@my365.bellevue.edu>
2020-02-23 11:40:37 -0600

    week 2 assignments

---
+++ week-2/Cords-header-test.js
@@ -0,0 +1,14 @@
+
+/*
+============================================
+; Title:  header-test.js
+; Author: Professor Krasso
+; Date:   23 Feb 2020
+; Modified by: Jason Cords
+; Description: Displays a formatted header
+;===========================================
+*/
+
+const header = require('../Cords-header.js');
+
+console.log(header.display("Jason", "Cords", "Week 2"));
